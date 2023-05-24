20695d8db953f53dfb97ed4e73ce71e04d4fc6d4
Prasathdv <prasath.dv@gmail.com>
2023-05-24 17:40:51 +0000

    HTML tenplates added

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'job_title': 'DevOps Engineer',
+  'location': 'Bengaluru, India',
+  'salary': 'Rs. 10,00,000'
+}, {
+  'id': 2,
+  'job_title': 'SRE',
+  'location': 'Hyderabad, India',
+  'salary': 'Rs. 12,00,000'
+}, {
+  'id': 3,
+  'job_title': 'Cloud Architect',
+  'location': 'Chennai, India'
+}, {
+  'id': 4,
+  'job_title': 'Cloud Engineer',
+  'location': 'Gurgaon, India',
+  'salary': 'Rs. 15,00,000'
+}]
 
 
 @app.route("/")
 def home():
-  return "<h1>Hello Flask</h1>"
+  return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
